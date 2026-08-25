f365fd11e1d61933332585e96ebc9915f492e746
alfred <mkchunmengalf@gmail.com>
2026-08-25 15:47:11 +0800

    added different datas under game files and added SCREEN_WIDTH value in data.py

--- data.py
+++ data.py
@@ -1,15 +1,25 @@
+import shutil
+
+SCREEN_WIDTH = shutil.get_terminal_size().columns
+
 def create_player():
     """Walks the player through a set of prompts before creating the
     player character.
     """
     # Get name
-    print("Hello, unnamed individual, what is your name?")
+    print("=" * SCREEN_WIDTH)
+    print("Character Creation: ")
+    print("=" * SCREEN_WIDTH)
+
+    print("- Hello, unnamed individual, what is your name?")
+    print("-" * SCREEN_WIDTH)
+
     name = input("> ")
     while name.isspace() or name is None:
         print("May I hear your name? ")
         name = input("> ")
     print(f"Alright! Hello {name}")
-
+    return name    
 
 def display(status: dict):
     """Takes in a status dict (returned by Game.status()) and formats
@@ -23,4 +33,18 @@ def prompt_player_choice(choices: list[str]):
     And displays them to the user for selection. The user is re-prompted
     If the choice is invalid. The player's choice is returned as a str.
     """
-    raise NotImplementedError
+    # Displays choices
+    print("- What will you do?")
+    for i, choice in enumerate(choices, start = 1):
+        print(f"  {i}. {choice}")
+    print("-" * SCREEN_WIDTH)
+
+    # Selection
+    chosen_num = input("> ")
+    while not chosen_num.isdigit() or int(chosen_num) < 1 or int(chosen_num) > len(choices):
+        print(f"[!] Invalid input, chosen number should be within 1 and {len(choices)}")
+        chosen_num = input("> ")
+
+    return choices[chosen_num - 1]
+
+    
