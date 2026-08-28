86ac00d66da435b09831119273124965bbb90586
Meng Pin <dan.meng.pin_2026@nyjc.edu.sg>
2026-08-28 05:12:15 +0000

    mook 5

--- game.py
+++ game.py
@@ -1,5 +1,98 @@
 import model
- money = model.Player()
+    
+
 class Game:
+    """Encapsulates the game logic, excluding room and character data.
+    """
+
+    def __init__(self):
+        self.play_area = Maze()
+        self.player = None
+        self.game_over = False
+        self.status = "alive"
+
+
+    def add_player(self, name: str) -> None:
+        """Adds the player into the game.
+        Only one player is supported for now.
+        Raises an error if more than one player is added.
+        """
+        if self.player != None:
+            raise error
+        else:
+            self.player = model.Player(name)
     
+
+    def is_gameover(self) -> bool:
+        """Returns True when the game should end, otherwise False."""
+        return self.game_over
+
+
+    def attack(self, predator, victim) -> None:
+        """guy attack guy"""
+        victim.set_hp(victim.get_hp() - predator.get_attack())
+        if not victim.check_alive():
+            if victim == self.player:
+                self.game_over = True
+            else:
+                del self.play_area.current_room.creatures[victim.name]
+            
+
+    def traverse(self, target: str) -> bool:
+        """travel to another room"""   
+        if target in self.play_area.rooms:
+            self.play_area.move_to_next_room(target)
+            return True
+        else:
+            return False
     
+    def get_options(self) -> list:
+        """Generates a list of choices that player can make.
+        Each choice is represented as a str.
+        """
+        options = []
+        for creature in self.play_area.current_room.creatures: #list
+            options.append("Attack " + creature.name)
+        for item in self.play_area.current_room.items: #list
+            options.append("Pick Up " + item.name)
+        for path in self.play_area.current_room.exits: # dict
+            options.append("Go " + path)
+        if len(self.player.inventory) <= 4:
+            for item in self.player.inventory:
+                options.append("Use " + name)
+        chosen = data.prompt_player_choice(options)
+        return options[chosen]
+    
+    def execute(self, choice: str) -> None:
+        if "Attack " in choice:
+            for creature in self.play_area.current_room.creatures:
+                if creature.name in choice:
+                    self.attack(self.Player, creature)
+        elif "Use " in choice:
+            for i in range(len(self.player.inventory)):
+                if self.player.inventory[i].name in choice:
+                    self.player.inventory[i].consume(self.player)
+                    if self.player.inventory[i] <= 0:
+                        self.player.inventory.pop(i)
+        elif "Pick Up "in choice:
+            for item in self.play_area.current_room.items:
+                if item.name in choice:
+                    self.player.inventory.append(item)
+        else:
+            self.traverse(choice[3:])
+
+
+    def get_status(self) -> dict:
+        return self.player.get_data()
+        
+
+    def welcome(self) -> None:
+        """Displays the welcome message for the start of the game."""
+        print("Welcome to Sample Game!")
+        print("=======================")
+
+
+def epilogue() -> None:
+    """Displays the game epilogue."""
+    print("==================================")
+    print("Thank you for playing Sample Game!")
--- model.py
+++ model.py
@@ -1,61 +1,89 @@
 class Player:
-        def __init(self, name: str, ):
-            self.name = name
-            self.health = 100
-            self.max_health = 100
-            self.inventory = [] #size 5
-            self.current_room = None
-
-    class Monster:
-        def __init__(self):
-            self.hp = 100
-            self.max_hp = 100
-            self.speed = 1 #undecided
-            self.attack = 5
-
-        def check_alive(self) -> bool:
-            if self.hp <= 0:
-                return False
-            return True
-
-        def get_hp(self) -> int:
-            return self.hp
-
-        def set_hp(self, new_hp: int) -> None:
-            self.hp = new_hp
-
-        def get_attack(self) -> int:
-            return self.attack
-
-        def set_attack(self, new_attack: int) -> None:
-            self.attack = new_attack
-
-        def take_damage(self, damage: int) -> None:
-            """Entity suffers damage"""
-            self.hp -= damage
+    def __init__(self, name: str):
+        self.name = name
+        self.max_health = 100
+        self.health = self.max_health
+        self.inventory = [] #size 5
+        self.current_room = None
+        self.attack = 1
     
+    def get_data(self) -> list:
+        data = []
+        data.append(self.name)
+        data.append(self.max_health)
+        data.append(self.health)
+        data.append(self.inventory)
+        data.append(self.current_room)
+        data.append(self.attack)
 
-    class Items:
-        
-        def __init__(self):
-            self.name = "Placeholder"
-            self.description = "Placeholder"
-            self.heal = None
-            self.attack_buff = None
-            self.vunerability = None
-            self.open_the_portal = False
-        
-    class Room:
-        def __init__(self, name: str, description: str, exits: dict, creatures: list, items: list):
-            self.name = name
-            self.description = description
-            self.exits = exits
-            self.creatures = creatures
-            self.items = items
+class Monster:
+    def __init__(self, name: str, data: dict):
+        self.name = name
+        self.max_hp = data["Health"]
+        self.hp = self.max_hp
+        self.description = data["Description"]
+        self.attack = data["Attack"]
+        self.attack_message = data["Attack_Message"]
+        self.defeat_message = data["Defeat_Message"]
 
-    class Maze:
-        def __init__(self):
-            self.rooms = []
-            self.start_room = Room()
-            self
-        
+    def check_alive(self) -> bool:
+        if self.hp <= 0:
+            return False
+        return True
+
+    def get_hp(self) -> int:
+        return self.hp
+
+    def set_hp(self, new_hp: int) -> None:
+        self.hp = new_hp
+
+    def get_attack(self) -> int:
+        return self.attack
+
+    def set_attack(self, new_attack: int) -> None:
+        self.attack = new_attack
+
+class Items:
+    
+    def __init__(self, name: str, data: dict):
+        self.name = name
+        self.description = data["Descripton"]
+        self.type = data["Type"]
+        self.amount = data["Amount"]
+        self.uses = data["Uses"]
+
+    def consume(self, target):
+        if self.type == "Healing":
+            target.hp = min(target.max_health, target.health + self.amount)
+        elif self.type == "Damage":
+            target.attack += self.amount
+
+
+class Room:
+    def __init__(self, data: dict):
+        self.name = data["Name"]
+        self.description = data["Description"]
+        self.exits = data["Exits"]
+        self.creatures = []
+        for creature, info in data["Creatures"]:
+            self.creatures.append(Monster(creature, info))
+        self.items = []
+        for item, info in data["Items"]:
+            self.item.append(Items(item, info))
+
+
+class Maze:
+    def __init__(self, rooms: dict, enemy: dict):
+        self.rooms = {}
+        for key, value in json.loads(rooms):
+            self.rooms[key] = Room(value)
+        self.current_room = self.rooms["Room 0"]
+    
+    def get_current_room(self) -> Room:
+        return self.current_room
+    
+    def move_to_next_room(self, next_room: str) -> None:
+        self.current_room = self.rooms[next_room]
+
+
+    
